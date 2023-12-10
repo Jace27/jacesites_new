94b804b10494605d596f845e4916a52f20d79355
dan27 <dan271@inbox.ru>
2023-12-10 04:19:21 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/save/{save}', function (string $save) { return view('saves.'.$save)
 Route::get('/practicals', function () { return view('practicals'); })->name('practicals');
 Route::get('/practical/{practical}', function (string $practical) { return view('practicals.'.$practical); });
 
-Route::get('/login', function () { return view('login'); })->name('login');
+Route::get('/login', function () { return redirect('/'); })->name('login');
 Route::get('/profile', function () { return view('profile'); })->middleware('auth')->name('profile');
 
 Route::get('/tools/randomizer', function () { return view('tools.randomizer'); });
